ed215f8f186ef25ec87405df7d96d5399451000b
BradK14 <brad14@csu.fullerton.edu>
2020-08-01 11:04:32 -0700

    Finished put, cleaned up get and added comments

--- serv.py
+++ serv.py
@@ -28,29 +28,46 @@ def receiveMessage(socket,numBytes):
 
     return buffer
 
-def get(message, clientSocket, machine):
+def get(message, clientSocket):
     message = message.split(" ",1)
     fileName = message[1]
     fileObj = open(fileName, "rb")
 
+    # Get file size
     counter = 0
     for chunk in iter(partial(fileObj.read, 64), b''):
         counter += 1
+
+    # Send file size to the client
     clientSocket.send(str(counter).encode())
     fileObj.close()
 
     # Get a go ahead to continue
     clientSocket.recv(1024).decode()
 
+    # Send the file to the client in chunks
     fileObj = open(fileName, "rb")
     for chunk in iter(partial(fileObj.read, 64), b''):
-        #clientSocket.sendto(str(chunk).encode(),(machine,listenPort))
         clientSocket.send(chunk)
     fileObj.close()
 
 
-def put():
-    return
+def put(socket):
+    # Let the client continue
+    socket.send('go ahead'.encode())
+
+    # Receive file size
+    counter = socket.recv(64).decode()
+
+    # Let the client continue
+    socket.send('go ahead'.encode())
+
+    # Receive and display file info looping by file size given by client
+    print("Message received:")
+    print(counter, " chunks")
+    for i in range(int(counter)):
+        print(socket.recv(64))
+
 def ls(socket):
 
     directoryContents = os.listdir(os.getcwd())
@@ -70,15 +87,14 @@ def ls(socket):
 
 clientSocket, address = welcomeSock.accept()
 print("Accepted connection from client: ", address)
-machine = clientSocket.recv(1024).decode()
 
 while(1):
     message = clientSocket.recv(1024).decode()
     if("get" in message):
-        get(message, clientSocket, machine)
+        get(message, clientSocket)
         print("Sent file info")
     elif("put" in message):
-        print("put")
+        put(clientSocket)
     elif(message == "ls"):
         ls(clientSocket)
         print("Sent directory info")
